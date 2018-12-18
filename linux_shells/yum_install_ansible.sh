b584ef01e1c17eaf9129c4e03bca51e474848d69
LANDAN <landanhsu@bpit888.com>
2018-12-18 17:23:22 +0800

    install pip and mysql-python

--- linux_shells/yum_install_ansible.sh
+++ linux_shells/yum_install_ansible.sh
@@ -1,2 +1,8 @@
 sudo yum -y install epel-release
 sudo yum -y install ansible
+sudo yum -y install epel-release
+sudo yum -y install python-pip
+sudo pip install --upgrade pip
+sudo yum install python-devel
+
+sudo pip install mysql-python
