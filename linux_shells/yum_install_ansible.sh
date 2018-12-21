46500851481b5c1f8e4e76d422c875febb616a7f
LANDAN <landanhsu@bpit888.com>
2018-12-21 14:32:23 +0800

    -y

--- linux_shells/yum_install_ansible.sh
+++ linux_shells/yum_install_ansible.sh
@@ -3,6 +3,6 @@ sudo yum -y install ansible
 sudo yum -y install epel-release
 sudo yum -y install python-pip
 sudo pip install --upgrade pip
-sudo yum install python-devel
+sudo yum install -y python-devel
 
-sudo pip install mysql-python
+#sudo pip install -y mysql-python
