24795d63ffcf5b2b3a3fba3ddd96c376d60ab9a1
LANDAN <landanhsu@bpit888.com>
2019-01-01 15:26:46 +0800

    Fixed: shell

--- linux_shells/yum_install_ansible.sh
+++ linux_shells/yum_install_ansible.sh
@@ -1,8 +1,7 @@
+sudo yum -y install python-pip
 sudo yum -y install epel-release
 sudo yum -y install ansible
-sudo yum -y install epel-release
-sudo yum -y install python-pip
 sudo pip install --upgrade pip
+#sudo /usr/bin/pip install ansible 
 sudo yum install -y python-devel
-
-#sudo pip install -y mysql-python
+#sudo /usr/bin/pip3.4 install ansible # cannt install ansible via pip3.0
