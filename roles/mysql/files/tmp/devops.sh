af74cd83d94784d74217f14d23afc6fcedebe9a5
landanhsu <landanhsu@bpit888.com>
2019-01-24 15:36:57 +0000

    mysql for devops

--- roles/mysql/files/tmp/devops.sh
+++ roles/mysql/files/tmp/devops.sh
@@ -5,3 +5,5 @@ mysqldbcompare --server1=root:mysql_pass_cli@127.0.0.1:3306 --server2=root:mysql
 mysqldiff --server1=root:mysql_pass_cli@127.0.0.1:3306 --server2=root:mysql_pass_router@10.0.0.22:3306 landan.users:landan.users
 
 mysqldump -uroot -T /tmp/ landan users --fields-terminated-by="," --where
+
+mysqlbinlog  mysql-bin.000007
