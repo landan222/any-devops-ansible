9c737df1dc277dc965e6140c0ddc4cad8e640c46
LANDAN <landanhsu@bpit888.com>
2019-01-24 22:01:08 +0800

    mysql

--- roles/mysql/files/tmp/devops.sh
+++ roles/mysql/files/tmp/devops.sh
@@ -1,1 +1,5 @@
 mysqlimport -uroot test /tmp/users.txt --fields-terminated-by="," --lines-terminated-by="\r\n"
+
+mysqldbcompare --server1=root:mysql_pass_cli@127.0.0.1:3306 --server2=root:mysql_pass_router@10.0.0.22:3306 landan:landan
+
+mysqldiff --server1=root:mysql_pass_cli@127.0.0.1:3306 --server2=root:mysql_pass_router@10.0.0.22:3306 landan.users:landan.users
