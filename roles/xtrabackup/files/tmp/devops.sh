e9ea005bb1e130457c15784906b0580d27aeea57
LANDAN <landanhsu@bpit888.com>
2019-02-06 18:21:39 +0800

    xtrabackup

--- roles/xtrabackup/files/tmp/devops.sh
+++ roles/xtrabackup/files/tmp/devops.sh
@@ -4,8 +4,8 @@
 ## xtrabackup 8.0 版本 适应 MySQL Server 8.0 与 Percona Server 8.0
 
 ######################################## xtrabackup 2.4 版本 ########################################
-innobackupex --user=backup --passwd=mysql_pass_backup --socket=/var/lib/mysql/mysql.sock --defaults- file=/etc/my.cnf /tmp/xtrabackup/full --no-timestamp
+sudo innobackupex --user=backup --passwd=mysql_pass_backup --socket=/var/lib/mysql/mysql.sock --defaults- file=/etc/my.cnf /tmp/xtrabackup/full --no-timestamp
 
 
 ######################################## xtrabackup 8.0 版本 ########################################
-xtrabackup --backup --host=localhost --user=backup --password=mysql_pass_backup
+sudo xtrabackup --backup --host=localhost --user=backup --password=mysql_pass_backup  --target-dir=/home/vagrant/www/xtrabackup
