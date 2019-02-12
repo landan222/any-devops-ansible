cb9f8df5120dbd64f4a11911df5e14d69a02b4b4
LANDAN <landanhsu@bpit888.com>
2019-02-13 05:01:21 +0800

    mysql , rid of useless code

--- roles/mysql/files/tmp/devops.sh
+++ roles/mysql/files/tmp/devops.sh
@@ -17,9 +17,3 @@ mysqldiff --server1=root:mysql_pass_cli@127.0.0.1:3306 --server2=root:mysql_pass
 mysqldump -uroot -T /tmp/ landan users --fields-terminated-by="," --where
 
 mysqlbinlog --no-defaults mysql-bin.000001 --database=landan --start-datetime='2019/01/01 00:00:00' --stop-datetime='2019/02/01 00:00:00' 
-
-
-
-sudo ndb_mgmd -f /etc/mgm.ini --reload
-
-sudo ndb_mgm -e "show"
