ce98acd71b281029674b014d3c989d4bd724f57e
LANDAN <landanhsu@bpit888.com>
2019-02-14 11:32:59 +0800

    node, include yarn

---
+++ roles/node/files/tmp/devops.sh
@@ -0,0 +1,20 @@
+npm install typescript
+yarn add typescript
+
+npm install typescript --no-bin-links
+yarn add typescript --no-bin-links
+
+npm install 
+yarn 
+
+npm install taco --save
+yarn add taco
+
+npm uninstall taco --save
+yarn remove taco
+
+npm install taco --save-dev
+yarn add taco --dev
+
+npm update --save
+yarn upgrade
