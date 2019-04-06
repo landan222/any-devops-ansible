126e9b1483c9b76d806888b85ad3f9d324465b80
landan345 <landan345.btl@gmail.com>
2019-04-06 18:11:19 +0800

    beanstalk

--- roles/supervisor/files/tmp/devops.sh
+++ roles/supervisor/files/tmp/devops.sh
@@ -9,15 +9,23 @@ sudo supervisorctl -c /etc/supervisor/supervisor.conf
 # 进入 supervisord CLI 控制界面
 sudo supervisorctl -c /etc/supervisor/supervisor.conf status
 
-# 进入 supervisord CLI 控制界面
-sudo supervisorctl -c /etc/supervisor/supervisor.conf read
+# 进入 supervisord CLI 控制界面  读取有更新（增加）的配置文件，不会启动新添加的程序
+sudo supervisorctl -c /etc/supervisor/supervisor.conf reread
 
-# 进入 supervisord CLI 控制界面
+# 进入 supervisord CLI 控制界面  重启配置文件修改过的程序
 sudo supervisorctl -c /etc/supervisor/supervisor.conf update
 
 
-# 重启 lottery-service_resource_backend-lumen php73_artisan_swoole_http_start
-sudo supervisorctl -c /etc/supervisor/supervisor.conf restart lottery-service_resource_backend-lumen:php73_artisan_swoole_http_start
+# 重启 lottery-service_resource_backend-lumen GROUP 所有 命令
+sudo supervisorctl -c /etc/supervisor/supervisor.conf restart lottery-service_resource_backend-lumen:
 
-# 重启 lottery-crawler_all_backend-sracpy python3_src_runner_lottery_issue_index
-sudo supervisorctl -c /etc/supervisor/supervisor.conf restart lottery-crawler_all_backend-sracpy:python3_src_runner_lottery_issue_index
+# 重启 lottery-crawler_all_backend-sracpy GROUP 所有 命令
+sudo supervisorctl -c /etc/supervisor/supervisor.conf restart lottery-crawler_all_backend-sracpy:
+
+
+
+# 关闭 lottery-service_resource_backend-lumen php73_artisan_swoole_http_start
+sudo supervisorctl -c /etc/supervisor/supervisor.conf stop lottery-service_resource_backend-lumen:php73_artisan_swoole_http_start
+
+# 启动 lottery-service_resource_backend-lumen php73_artisan_swoole_http_start
+sudo supervisorctl -c /etc/supervisor/supervisor.conf start lottery-service_resource_backend-lumen:php73_artisan_swoole_http_start
